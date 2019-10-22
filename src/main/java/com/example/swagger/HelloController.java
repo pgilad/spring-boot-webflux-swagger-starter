eb13f6c0fac8fd8ed800b4b899e26bf06f0d352b
Gilad Peleg <giladp007@gmail.com>
2019-10-23 00:51:40 +0300

    Update controller example and readme picture

--- src/main/java/com/example/swagger/HelloController.java
+++ src/main/java/com/example/swagger/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.swagger;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import static java.util.Objects.requireNonNullElse;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public Mono<String> sayHi() {
-        return Mono.just("Hi");
+    @GetMapping("/hello/get")
+    public Mono<String> getHello() {
+        return Mono.just("Getting back..");
+    }
+
+    @PostMapping("/hello/post")
+    public Mono<String> postHello() {
+        return Mono.just("Posting back..");
+    }
+
+    @GetMapping("/person")
+    public Mono<Person> getPerson(String name, Integer age) {
+        var personName = requireNonNullElse(name, "Anonymous");
+        var personAge = requireNonNullElse(age, 21);
+        var person = new Person(personName, personAge);
+
+        return Mono.just(person);
     }
 }
---
+++ src/main/java/com/example/swagger/Person.java
@@ -0,0 +1,12 @@
+package com.example.swagger;
+
+@SuppressWarnings("WeakerAccess")
+class Person {
+    public String name;
+    public Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
